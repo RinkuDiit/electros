62f7a7eb8a4a2f8e9a925954af37d1bc046e73d7
Manjeet <jaaniraj283@gmail.com>
2024-05-11 12:54:45 +0530

    test

---
+++ src/components/Shiping.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Shiping() {
+  return (
+    <div>
+        <div className='spngsecurepayment'>
+            <div className='fastshping'></div>
+            <div className='fastsupport'></div>
+            <div className='return30days'></div>
+            <div className='securepayment'></div>
+        </div>
+    </div>
+  )
+}
+
+export default Shiping
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import Main from './components/img/Main';
+import Shiping from './components/Shiping';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <App /> */}
+    <Shiping />
   </React.StrictMode>
 );
 
