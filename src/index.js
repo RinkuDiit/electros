01c39418deb76619d2f0bff59017373967e3504d
Manjeet <jaaniraj283@gmail.com>
2024-05-11 12:56:43 +0530

    test1

--- src/index.js
+++ src/index.js
@@ -3,24 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
 // import Main from './components/img/Main';
 import Shiping from './components/Shiping';
 
-=======
 import Product from './components/Product'
 import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> 744c926838569ffed91eee4ad58cd5110ba4fb0c
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-<<<<<<< HEAD
     <Shiping />
-=======
     <Product />
->>>>>>> 744c926838569ffed91eee4ad58cd5110ba4fb0c
   </React.StrictMode>
 );
 
