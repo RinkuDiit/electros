670f2c5ebd49e4365ad5aa8beaee629a2c510938
Rinku <rr1341570@gmail.com>
2024-05-13 11:28:12 +0530

    product page

--- src/components/product.js
+++ src/components/product.js
@@ -24,19 +24,41 @@ function Product() {
           </div>
           <div className='col-xl-12 bottum_line'>
             <p className='h2'>$189.00</p>
-            <p className='h4'>The soft headphone cushion makes the whole experience comfortable. The experience of listening is changed by the use of this beauty. It is light and compact with a beautiful look and feel to the whole headphone.</p>
+            <p className='h4 P_productpage'>The soft headphone cushion makes the whole experience comfortable. The experience of listening is changed by the use of this beauty. It is light and compact with a beautiful look and feel to the whole headphone.</p>
           </div>
           <div className='col-xl-12 bottum_line'>
-          <i class="fa fa-facebook-official" aria-hidden="true"></i></div>
+          <i className="icone_fa fa fa-facebook-official fa-2x" aria-hidden="true" style={{color:'blue'}}></i>
+          <i className=" icone_fa fa fa-whatsapp fa-2x " aria-hidden="true" style={{color:'green'}}></i>
+          <i className="icone_fa fa fa-twitter fa-2x" aria-hidden="true" style={{color:'blue'}}></i>
+          </div>
           <div className='col-xl-12 bottum_line'>
-            <div className=' col-md-6 fs-5' style={{ backgroundColor:'#364253',color:'white',borderRadius:'5px'}}><button className='btn Cardbtn fs-5'>Add to Card </button><i class="fa fa-shopping-cart" aria-hidden="true"></i></div>
+            
+            <div className=' col-md-6 fs-5' style={{ backgroundColor:'#364253',color:'white',borderRadius:'5px'}}><button className='btn Cardbtn fs-5'>Add to Card </button><i className="fa fa-shopping-cart" aria-hidden="true"></i></div>
           </div>
         </div>
     </div>
+
+    </div>
     
   </div>
+
+  <div style={{border:"solid 1px black"}} className="row ">
+          <div className='container'>
+             <div className='row'>
+             <div className="col-xl-12">
+          <ul class="list-group">
+            <li class="list-group"><h4>Rating</h4></li>
+            <li class="list-group"><h3><i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"> </i></h3></li>
+            <li class="list-group">0.0/5 (No Rating)</li>
+            <li class="list-group"><h4>Reviews</h4></li>
+            <li class="list-group">There aren't any reviews to display.</li>
+          </ul>
+          </div>
+             </div>
+          </div>
+        </div>
 </div>
-</div>
+
 
 
   )
