61bc2f0eae97ffb78011f90c849b8c70e98e69c6
RAHUL <rahulrahulkumar36742@gmail.com>
2024-05-14 12:31:17 +0530

    rahull

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Product from './components/Product'
+// import Product from './components/Product';
+  
+  import Rahul1 from '../src/components/Rahul1';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 
@@ -12,7 +14,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Product />
+    {/* <Product />
+     */}
+
+     <Rahul1 />
   </React.StrictMode>
 );
 
