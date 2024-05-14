43bf2d4549e62b497e33937a865064ae4dbd46cd
Rinku <rr1341570@gmail.com>
2024-05-14 12:24:53 +0530

    card

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,36 +4,46 @@ function Card() {
   return (
     <div>
         <div class="container card_container">
-  <div class="row">
-    <div class="col-md-6">
-      item
+  <div style={{border:'solid 1px '}} class="row">
+    <div class="col-md-6 col-sm-12">
+      Item
+      <div class="col-md-12">
+      <div>
+        <img style={{width:'40%',height:'40%'}} src='https://gadgetszone-demo.zohocommerce.com/product-images/700x700-05.jpg/2379410000000072470/700x700' />
+      </div>
     </div>
-    <div class="col-md-2">
+    </div>
+    <div class="col-md-2 col-sm-12">
       Quantity
+
+      <div class="col-md-12">
+      <div className='additmemore'>
+      <div  style={{width:'20%', float:'left' ,margin:'11px 0px'}} > <button className='btnadd'>-</button></div> 
+       <div style={{width:'60%', float:'left', margin:'11px 0px'}}><p className='h6'>0</p></div>      
+      <div  style={{width:'20%', float:'left' ,margin:'11px 0px'}} > <button className='btnadd'>+</button></div>
+      </div>
     </div>
-    <div class="col-md-2">
+    </div>
+    <div class="col-md-2 col-sm-12">
       Subtotal
+      <div class="col-md-12">
+      <p className='h5'>$220.00</p>
     </div>
-    <div class="col-md-2">
+    </div>
+    <div class="col-md-2 col-sm-12">
       Remove
+
+      <div class="col-md-12">
+    <i class="fa fa-trash-o" aria-hidden="true"></i>
+    </div>
     </div>
   </div>
 
-  <div class="row">
-    <div class="col-md-6">
-      <div>
-        <img src='' />
-      </div>
-    </div>
-    <div class="col-md-2">
-      Quantity
-    </div>
-    <div class="col-md-2">
-      Subtotal
-    </div>
-    <div class="col-md-2">
-      Remove
-    </div>
+  <div style={{border:'solid 1px '}} class="row">
+    
+  
+    
+    
   </div>
 </div>
     </div>
