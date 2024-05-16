1a9a151ce254617ed07cbf474934fd291091f83b
Rinku <rr1341570@gmail.com>
2024-05-16 11:30:08 +0530

    footer

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ import Contact from './components/Contact';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App /> 
+    {/* <App />  */}
     <App />
     {/* <Product /> */}
     {/* <App /> */}
